d74b428555d8fab22f95de3b7569f087b2960e76
Ayush Jain <ayushmjain@google.com>
2024-04-01 06:08:14 +0000

    init commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  load_balancer_port_name = var.load_balancer_port != null ? "load-balancer-port" : null
+
+  # Construct the script to set environment variables
+  env_script = join("\n", [for k, v in var.env_variables : "export ${k}='${v}'"])
+
+  # Combine the environment variables script with the user's script
+  combined_startup_script = "${local.env_script}\n\n${var.startup_script}"
+}
+
+data "google_compute_zones" "available" {
+  project = var.project_id
+  region  = var.region
+}
+
+module "instance_template" {
+  source  = "terraform-google-modules/vm/google///modules/instance_template"
+  version = "8.0.1"
+
+  name_prefix      = "${var.managed_instance_group_name}-template-"
+  source_image     = var.vm_image
+  network = var.network_name
+  service_account = {
+    email  = var.service_account_email
+    scopes = ["cloud-platform"]
+  }
+  startup_script = local.combined_startup_script
+}
+
+module "mig" {
+  source  = "terraform-google-modules/vm/google//modules/mig"
+  version = "8.0.1"
+
+  project_id                = var.project_id
+  mig_name                  = var.managed_instance_group_name
+  hostname                  = var.managed_instance_group_name
+  instance_template         = module.instance_template.self_link
+  region                    = var.region
+  distribution_policy_zones = data.google_compute_zones.available.names
+  autoscaling_enabled       = true
+  named_ports = [
+    {
+      name = local.load_balancer_port_name
+      port = var.load_balancer_port
+    },
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  description = "The project ID to deploy the GCE VMs to"
+  type        = string
+}
+
+variable "region" {
+  description = "The region where the GCE VMs should be deployed"
+  type = string
+}
+
+variable "managed_instance_group_name" {
+  description = "Name of the managed instance group"
+  type = string
+}
+
+variable "vm_image" {
+  description = "The image from which to initialize the VM disk"
+  default = null
+  type = string
+}
+
+variable "network_name" {
+  type = string
+  default = null
+  description = "VPC network name where the GCE VMs are created"
+}
+
+variable "service_account_email" {
+  type = string
+  default = null
+  description = "Email of the service account that should be attached to the GCE VMs"
+}
+
+variable "env_variables" {
+  type = map(string)
+  default = {}
+  description = "Environment variables for GCE VMs"
+}
+
+variable "startup_script" {
+  type = string
+  default = ""
+  description = "Startup script for GCE VMs"
+}
+
+variable "load_balancer_port" {
+  type = string
+  default = null
+  description = "Port for load balancer to connect to the VM instances"
+}
